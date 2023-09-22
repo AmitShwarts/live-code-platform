d4c9a886912b8a964c5aff1b2f40292f5b9268ce
Amit Shwarts <amit.shwarts@gmail.com>
2023-09-22 15:03:43 +0300

    feat: init server (#9)

--- packages/live-code-platform-server/src/index.ts
+++ packages/live-code-platform-server/src/index.ts
@@ -1,1 +1,31 @@
-export {};
+import express from 'express';
+import http from 'http';
+import { WebSocket } from 'ws';
+import kleur from 'kleur';
+
+const PORT = '8080'
+
+const app = express();
+const server = http.createServer(app);
+const socket = new WebSocket.Server({ server });
+
+socket.on('connection', (ws: any) => {
+  // eslint-disable-next-line no-console
+  console.log('Client connected');
+
+  ws.on('message', (message: string) => {
+    // eslint-disable-next-line no-console
+    console.log(`Received: ${message}`);
+    ws.send(`You sent: ${message}`);
+  });
+
+  ws.on('close', () => {
+    // eslint-disable-next-line no-console
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(kleur.yellow(`Server is listening on http://localhost:${PORT}`));
+});
